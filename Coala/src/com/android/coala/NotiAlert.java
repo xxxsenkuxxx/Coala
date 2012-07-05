3bf7e6dd141d858d8cc6d79f6abede3921623adf
senku <senku7@gmail.com>
2012-07-06 02:04:47 +0900

    image

--- Coala/src/com/android/coala/NotiAlert.java
+++ Coala/src/com/android/coala/NotiAlert.java
@@ -5,27 +5,35 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class NotiAlert extends Activity implements View.OnClickListener {
 
 	NotificationManager noti;
 
-	private Button callBTN, waitBTN;
+	private ImageButton callBTN, waitBTN;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
 		setContentView(R.layout.noti_alert);
 
 		noti = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-		callBTN = (Button) findViewById(R.id.alert_call);
+		callBTN = (ImageButton) findViewById(R.id.alert_call);
 		callBTN.setOnClickListener(this);
-		waitBTN = (Button) findViewById(R.id.alert_wait);
+		waitBTN = (ImageButton) findViewById(R.id.alert_wait);
 		waitBTN.setOnClickListener(this);
 	}
 
@@ -41,9 +49,9 @@ public class NotiAlert extends Activity implements View.OnClickListener {
 			PendingIntent pendingIntent = PendingIntent.getActivity(NotiAlert.this, 0,
 					new Intent(NotiAlert.this, MemberListActivity.class), 0);
 
-			Notification notifi = new Notification(R.drawable.coala_icon,
-					"전화", System.currentTimeMillis());
-			notifi.setLatestEventInfo(NotiAlert.this, "Coala", "연락을 기다리고있어요",
+			Notification notifi = new Notification(R.drawable.noti_icon,
+					"전화알림", System.currentTimeMillis());
+			notifi.setLatestEventInfo(NotiAlert.this, "Coala", "연락을 기다리고 있어요",
 					pendingIntent);
 			notifi.flags = Notification.FLAG_AUTO_CANCEL;
 			noti.notify(0, notifi);
